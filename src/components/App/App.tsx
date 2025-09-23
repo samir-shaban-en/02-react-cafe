752e29d95241127edbcf5b3bc6fe50e09e52d96b
Samir Shaban <samirshabanpsn@gmail.com>
2025-09-23 13:57:56 +0200

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import CafeInfo from '../CafeInfo/CafeInfo';
+import css from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
+};
+
+export default App;
