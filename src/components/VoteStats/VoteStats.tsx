985611c509a43816975eeefedd67d644ef8e3554
Samir Shaban <samirshabanpsn@gmail.com>
2025-09-24 21:12:38 +0200

    import type Votes in VoteStats

--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,10 +1,7 @@
 import styles from './VoteStats.module.css';
+import { type Votes } from '../../types/votes';
 interface VoteStatsProps {
-  votes: {
-    good: number;
-    neutral: number;
-    bad: number;
-  };
+  votes: Votes;
   totalVotes: number;
   positiveRate: number;
 }
